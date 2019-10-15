bbc4f3cbca10a6026e993f0c6fc9675c15932f0a
Liu Ke (RBJP/PAA2) <lik1sby@bosch.com>
2019-10-15 15:30:46 +0900

    added some reference URLs to ErrorHandlingTest project

--- ErrorHandlingTest/src/main/java/com/bluewind/errorhandling/ErrorHandlingTestApplication.java
+++ ErrorHandlingTest/src/main/java/com/bluewind/errorhandling/ErrorHandlingTestApplication.java
@@ -3,6 +3,11 @@ package com.bluewind.errorhandling;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * https://www.tutorialspoint.com/spring_boot/spring_boot_exception_handling.htm
+ * https://www.baeldung.com/spring-boot-custom-error-page
+ */
+
 @SpringBootApplication
 public class ErrorHandlingTestApplication {
 
