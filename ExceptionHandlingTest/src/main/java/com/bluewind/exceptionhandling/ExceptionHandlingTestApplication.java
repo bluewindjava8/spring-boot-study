4d05945a4233d2a872a0d8bac10f31c98fb6f343
Liu Ke (RBJP/PAA2) <bluewind@hotmail.com>
2019-10-18 08:58:58 +0900

    Added ExceptionHandler project

---
+++ ExceptionHandlingTest/src/main/java/com/bluewind/exceptionhandling/ExceptionHandlingTestApplication.java
@@ -0,0 +1,17 @@
+package com.bluewind.exceptionhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+ * https://www.tutorialspoint.com/spring_boot/spring_boot_exception_handling.htm
+ * https://www.baeldung.com/spring-boot-custom-error-page
+ * https://howtodoinjava.com/spring-boot2/spring-rest-request-validation/
+ */
+@SpringBootApplication
+public class ExceptionHandlingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionHandlingTestApplication.class, args);
+	}
+
+}
---
+++ ExceptionHandlingTest/src/main/java/com/bluewind/exceptionhandling/exception/ProductNotfoundException.java
@@ -0,0 +1,5 @@
+package com.bluewind.exceptionhandling.exception;
+
+public class ProductNotfoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+}
