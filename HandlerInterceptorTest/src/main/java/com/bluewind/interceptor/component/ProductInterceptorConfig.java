cd741830159a4ebfe2f9e3c49eb83c60eff1550d
Liu Ke (RBJP/PAA2) <bluewind@hotmail.com>
2019-10-18 16:15:32 +0900

    added a second handler interceptor in HandlerInterceptor project

--- HandlerInterceptorTest/src/main/java/com/bluewind/interceptor/component/ProductInterceptorConfig.java
+++ HandlerInterceptorTest/src/main/java/com/bluewind/interceptor/component/ProductInterceptorConfig.java
@@ -9,9 +9,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class ProductInterceptorConfig implements WebMvcConfigurer {
 	@Autowired
 	ProductHandlerInterceptor productControllerInterceptor;
+	
+	@Autowired
+	MyHandlerInterceptor myHandlerInterceptor;
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
+		//registry of interceptors is necessary for interceptors to work
 		registry.addInterceptor(productControllerInterceptor);
+		//Add a second handler interceptor
+		registry.addInterceptor(myHandlerInterceptor);
+		
+		/* The execution order of interceptor is as below:
+		 *  Pre Handle method is Calling
+			Pre Handle method is Calling in MyHandlerInterceptor
+			Post Handle method is Calling in MyHandlerInterceptor
+			Post Handle method is Calling
+			Request and Response is completed in MyHandlerInterceptor
+			Request and Response is completed
+		 */
 	}
 }
