f403a29e59965cdcaef4425982d8689dc08060c7
Liu Ke (RBJP/PAA2) <bluewind@hotmail.com>
2019-10-24 22:30:44 +0900

    adjusted a wrong concept of the relation between actuator and spring security

--- ActuatorTestWithoutSecurity/src/main/java/com/bluewind/actuator/ActuatorTestWithoutSecurityApplication.java
+++ ActuatorTestWithoutSecurity/src/main/java/com/bluewind/actuator/ActuatorTestWithoutSecurityApplication.java
@@ -3,6 +3,10 @@ package com.bluewind.actuator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * Actuator does not have any dependency on spring security. 
+ * Actuator only exposes several entry points like /actuator/metrics, /actuator/beans, etc
+ */
 @SpringBootApplication
 public class ActuatorTestWithoutSecurityApplication {
 
