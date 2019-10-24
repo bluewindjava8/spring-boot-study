b70efcf1c7a6da7950da3265cdbfbc9e30d1b9e9
Liu Ke (RBJP/PAA2) <bluewind@hotmail.com>
2019-10-24 22:29:11 +0900

    added some security config in ActuatorTest project

--- ActuatorTest/src/main/java/com/bluewind/actuator/config/SpringSecurityConfig.java
+++ ActuatorTest/src/main/java/com/bluewind/actuator/config/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.bluewind.actuator.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -11,8 +12,15 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
     	//Here, password("{noop}admin") means we don't use password encoder.
-        auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}blue").roles("ADMIN123");
         
  
     }
+    
+    
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//    	//permit all request( no need to authenticate)
+//        http.authorizeRequests().antMatchers("/**").permitAll().anyRequest().authenticated().and().csrf().disable();
+//    }
 }
